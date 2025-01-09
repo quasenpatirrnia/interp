dd7a959766e3a219f590363baead83dad74e7abd
quasenpatirrnia <125682020+quasenpatirrnia@users.noreply.github.com>
2025-01-09 10:54:19 -0800

    Update lex.c
    implement Identifier and Keyworeds

--- lex.c
+++ lex.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "DFA.h"
 
 typedef struct {
-	char *text;
+	char text[];
 	int pos;
 } Scanner;
 
@@ -48,9 +52,46 @@ typedef enum {
 	UNSIGNED,
 	VOID,
 	VOLATILE,
+	WHILE,
 } Keyword;
 
 typedef struct {
 	TokenType type;
 	char *value;
 } Token;
+
+int KeywordMatch(char s[]) {
+	char keywords[][32] = {
+		"auto", "break", "case", "char", 
+		"const", "continue", "default", "do",
+		"double", "else", "enum", "extern",
+		"float", "for", "goto", "if",
+		"int", "long", "register", "return",
+		"short", "signed", "sizeof", "static",
+		"struct", "switch", "typedef", "union",
+		"unsigned", "void", "volatile", "while"}
+	int i = 0;
+	int ret;
+	while ((ret = strncmp(s, keywords[i], 8)) && (i < 32))
+		i++;
+	return ret;
+}
+
+int IdentifierMatch(char s[], Arena *parent) {
+	int transitions[2][128];
+	int accepting = 1;
+	DFA *IdentifierDFA = NewDFA(parent, &accepting, 1, transitions, 2);
+	for (char c = 'a'; c <= 'z'; c++) {
+		AddTransition(IdentifierDFA, c, 0, 1);
+		AddTransition(IdentifierDFA, c, 1, 1);
+	}
+	for (char c = 'A'; c <= 'Z'; c++) {
+		AddTransition(IdentifierDFA, c, 0, 1);
+		AddTransition(IdentifierDFA, c, 1, 1);
+	}
+	for (char c = '0'; c <= '9'; c++)
+		AddTransition(IdentifierDFA, c, 0, 1);
+	AddTransition(IdentifierDFA, '_', 1, 1);
+	AddTransition(IdentifierDFA, '_', 0, 1);
+	return accepts(automaton, 0, s, 0);
+}
